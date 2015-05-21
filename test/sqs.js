3c25c5689a27238b2b32e22b7c4f4ee0540e3b2e
Steven Zeiler <zeiler.steven@gmail.com>
2015-05-21 09:38:20 -0700

    [FEATURE] Structure for ActiveJob.SQS.Base

---
+++ test/sqs.js
@@ -0,0 +1,22 @@
+import ActiveJob from '../lib'
+
+class MyQueue extends ActiveJob.SQS.Base {
+
+  type() { return 'xrp-payouts' }
+
+  perform(args, callback) {
+    console.log('pay the XRP', args)
+
+    callback('success')
+  }
+}
+
+MyQueue.performLater({
+  amount: 15000,
+  recipient: 'r4EwBWxrx5HxYRyisfGzMto3AT8FZiYdWk'
+})
+.then(console.log)
+.catch(console.log)
+
+//new ActiveJob.Worker(MyQueue).start()
+
